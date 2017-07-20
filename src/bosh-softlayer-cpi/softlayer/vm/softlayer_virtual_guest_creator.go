46fc8627290c487a75ddf07047460048163fd5c4
Zida <shawzida@gmail.com>
2017-07-20 18:12:02 +0800

    Migrate resize CPU/mem function from bosh_community

--- src/bosh-softlayer-cpi/softlayer/vm/softlayer_virtual_guest_creator.go
+++ src/bosh-softlayer-cpi/softlayer/vm/softlayer_virtual_guest_creator.go
@@ -15,6 +15,9 @@ import (
 	sl "github.com/maximilien/softlayer-go/softlayer"
 
 	"bosh-softlayer-cpi/util"
+	"strings"
+	"math"
+	"strconv"
 )
 
 type softLayerVirtualGuestCreator struct {
@@ -65,6 +68,228 @@ func (c *softLayerVirtualGuestCreator) Create(agentID string, stemcell bslcstem.
 }
 
 func (c *softLayerVirtualGuestCreator) GetAgentOptions() AgentOptions { return c.agentOptions }
+
+func (c *softLayerVirtualGuestCreator) UpgradeInstanceConfig(id int, cpu int, memory int, network int, privateCPU bool) error {
+	var err error
+	until := time.Now().Add(time.Duration(1) * time.Hour)
+	if err = c.WaitInstanceHasNoneActiveTransaction(id, until); err != nil {
+		return bosherr.WrapError(err, "Waiting until instance has none active transaction before os_reload")
+	}
+
+	_, err = c.UpgradeInstance(id, cpu, memory, network, privateCPU, 0)
+	if err != nil {
+		if strings.Contains(err.Error(), "A current price was provided for the upgrade order") {
+			return nil
+		}
+		return bosherr.WrapErrorf(err, "Upgrading configuration to virutal guest of  id '%d'", id)
+	}
+
+	until = time.Now().Add(time.Duration(1) * time.Hour)
+	if err = c.WaitInstanceHasActiveTransaction(id, until); err != nil {
+		return bosherr.WrapError(err, "Waiting until instance has active transaction after upgrading instance")
+	}
+
+	until = time.Now().Add(time.Duration(1) * time.Hour)
+	if err = c.WaitInstanceHasNoneActiveTransaction(id, until); err != nil {
+		return bosherr.WrapError(err, "Waiting until instance has none active transaction after upgrading instance")
+	}
+
+	until = time.Now().Add(time.Duration(1) * time.Hour)
+	if err = c.WaitInstanceUntilReady(id, until); err != nil {
+		return bosherr.WrapError(err, "Waiting until instance is ready after os_reload")
+	}
+
+	return nil
+}
+
+func (c *softLayerVirtualGuestCreator) WaitInstanceUntilReady(id int, until time.Time) error {
+	virtualGuestService, err := c.softLayerClient.GetSoftLayer_Virtual_Guest_Service()
+	if err != nil {
+		return bosherr.WrapError(err, "Creating VirtualGuestService from SoftLayer client")
+	}
+	for {
+		virtualGuest, err := virtualGuestService.GetObject(id)
+		if err != nil {
+			return err
+		}
+		if virtualGuest.AccountId == -1 {
+			return bosherr.WrapErrorf(err, "SoftLayer virtual guest '%d' does not exists", id)
+		}
+
+		lastReload, err := virtualGuestService.GetLastTransaction(id)
+		if err != nil {
+			return err
+		}
+		activeTxn, err := virtualGuestService.GetActiveTransaction(id)
+		if err != nil {
+			return err
+		}
+
+		virtualGuestService.GetPowerState(id)
+
+		reloading := activeTxn != datatypes.SoftLayer_Provisioning_Version1_Transaction{} &&
+			lastReload != datatypes.SoftLayer_Provisioning_Version1_Transaction{} &&
+			activeTxn.Id == lastReload.Id
+		if !reloading {
+			powerState, err := virtualGuestService.GetPowerState(id)
+			if err != nil {
+				return err
+			}
+			if powerState.KeyName == "RUNNING" {
+				return nil
+			}
+		}
+
+		now := time.Now()
+		if now.After(until) {
+			return bosherr.Errorf("Power on virtual guest with id %d Time Out!", virtualGuest.Id)
+		}
+
+		min := math.Min(float64(10.0), float64(until.Sub(now)))
+		time.Sleep(time.Duration(min) * time.Second)
+	}
+}
+
+func (c *softLayerVirtualGuestCreator) WaitInstanceHasActiveTransaction(id int, until time.Time) error {
+	virtualGuestService, err := c.softLayerClient.GetSoftLayer_Virtual_Guest_Service()
+	if err != nil {
+		return bosherr.WrapError(err, "Creating VirtualGuestService from SoftLayer client")
+	}
+	for {
+		trans, err := virtualGuestService.GetActiveTransactions(id)
+		if err != nil {
+			return bosherr.WrapError(err, "Getting active transactions from SoftLayer client")
+		}
+		if len(trans) != 0 {
+			return nil
+		}
+
+		now := time.Now()
+		if now.After(until) {
+			return bosherr.Errorf("Wait instance with id of '%d' has active transaction time out", id)
+		}
+
+		min := math.Min(float64(5.0), float64(until.Sub(now)))
+		time.Sleep(time.Duration(min) * time.Second)
+	}
+}
+
+func (c *softLayerVirtualGuestCreator) WaitInstanceHasNoneActiveTransaction(id int, until time.Time) error {
+	virtualGuestService, err := c.softLayerClient.GetSoftLayer_Virtual_Guest_Service()
+	if err != nil {
+		return bosherr.WrapError(err, "Creating VirtualGuestService from SoftLayer client")
+	}
+
+	for {
+		trans, err := virtualGuestService.GetActiveTransactions(id)
+		if err != nil {
+			return bosherr.WrapError(err, "Getting active transactions from SoftLayer client")
+		}
+
+		if len(trans) == 0 {
+			return nil
+		}
+
+		now := time.Now()
+		if now.After(until) {
+			return bosherr.Errorf("Waiting instance with id of '%d' has none active transaction time out", id)
+		}
+
+		min := math.Min(float64(5.0), float64(until.Sub(now)))
+		time.Sleep(time.Duration(min) * time.Second)
+	}
+}
+
+func (c *softLayerVirtualGuestCreator) UpgradeInstance(id int, cpu int, memory int, network int, privateCPU bool, additional_diskSize int) (*datatypes.SoftLayer_Container_Product_Order_Receipt, error) {
+	upgradeOptions := make(map[string]int)
+	public := true
+	if cpu != 0 {
+		upgradeOptions["guest_core"] = cpu
+	}
+	if memory != 0 {
+		upgradeOptions["ram"] = memory / 1024
+	}
+	if network != 0 {
+		upgradeOptions["port_speed"] = network
+	}
+	if privateCPU == true {
+		public = false
+	}
+
+	productPackageService, err := c.softLayerClient.GetSoftLayer_Product_Package_Service()
+	if err != nil {
+		return &datatypes.SoftLayer_Container_Product_Order_Receipt{}, bosherr.WrapError(err, "Creating VirtualGuestService from SoftLayer client")
+	}
+
+	packageType := "VIRTUAL_SERVER_INSTANCE"
+	productPackages, err := productPackageService.GetPackagesByType("packageType")
+
+	if err != nil {
+		return &datatypes.SoftLayer_Container_Product_Order_Receipt{}, err
+	}
+	if len(productPackages) == 0 {
+		return &datatypes.SoftLayer_Container_Product_Order_Receipt{}, bosherr.Errorf("No package found for type: %s", packageType)
+	}
+	packageID := productPackages[0].Id
+	packageItems, err := productPackageService.GetItems(packageID, "")
+	if err != nil {
+		return &datatypes.SoftLayer_Container_Product_Order_Receipt{}, err
+	}
+	var prices = make([]datatypes.SoftLayer_Product_Item_Price, 0)
+	for option, value := range upgradeOptions {
+		priceID := getPriceIdForUpgrade(packageItems, option, value, public)
+		if priceID == -1 {
+			return &datatypes.SoftLayer_Container_Product_Order_Receipt{},
+				bosherr.Errorf("Unable to find %s option with %d", option, value)
+		}
+		prices = append(prices, datatypes.SoftLayer_Product_Item_Price{Id: priceID})
+	}
+
+	if additional_diskSize != 0 {
+		diskItemPrice, err := c.getUpgradeItemPriceForSecondDisk(id, additional_diskSize)
+		if err != nil {
+			return &datatypes.SoftLayer_Container_Product_Order_Receipt{}, err
+		}
+		prices = append(prices, *diskItemPrice)
+	}
+
+	if len(prices) == 0 {
+		return &datatypes.SoftLayer_Container_Product_Order_Receipt{}, bosherr.Error("Unable to find price for upgrade")
+	}
+	order := datatypes.SoftLayer_Container_Product_Order{
+		ComplexType: "SoftLayer_Container_Product_Order_Virtual_Guest_Upgrade",
+		Prices:      prices,
+		Properties: []datatypes.Property{
+			{
+				Name:  "MAINTENANCE_WINDOW",
+				Value: time.Now().UTC().Format(time.RFC3339),
+			},
+			{
+				Name:  "NOTE_GENERAL",
+				Value: "Upgrade instance configuration.",
+			},
+		},
+		VirtualGuests: []datatypes.VirtualGuest{
+			{
+				Id: id,
+			},
+		},
+		PackageId: packageID,
+	}
+
+	upgradeOrder := datatypes.SoftLayer_Container_Product_Order_Virtual_Guest_Upgrade(order)
+
+	productOrderService, err := c.softLayerClient.GetSoftLayer_Product_Order_Service()
+	if err != nil {
+		return &datatypes.SoftLayer_Container_Product_Order_Receipt{}, bosherr.WrapError(err, "Creating ProductOrderService from SoftLayer client")
+	}
+	orderReceipt, err := productOrderService.PlaceContainerOrderVirtualGuestUpgrade(upgradeOrder)
+	if err != nil {
+		return &datatypes.SoftLayer_Container_Product_Order_Receipt{}, err
+	}
+
+	return &orderReceipt, nil
+}
 
 // Private methods
 func (c *softLayerVirtualGuestCreator) createBySoftlayer(agentID string, stemcell bslcstem.Stemcell, cloudProps VMCloudProperties, networks Networks, env Environment) (VM, error) {
@@ -187,6 +412,8 @@ func (c *softLayerVirtualGuestCreator) createByOSReload(agentID string, stemcell
 		return nil, bosherr.WrapError(err, "Failed to reload OS")
 	}
 
+	c.resizeByOrder(virtualGuest, vm.ID(), cloudProps)
+
 	err = UpdateDeviceName(virtualGuest.Id, virtualGuestService, cloudProps)
 	if err != nil {
 		return nil, err
@@ -257,4 +484,112 @@ func (c *softLayerVirtualGuestCreator) createByOSReload(agentID string, stemcell
 		}
 	}
 	return vm, nil
+}
+
+func (c *softLayerVirtualGuestCreator) resizeByOrder(vm datatypes.SoftLayer_Virtual_Guest, cid int, cloudProps VMCloudProperties) error {
+
+	if vm.StartCpus != cloudProps.StartCpus || vm.MaxMemory != cloudProps.MaxMemory ||
+		vm.DedicatedAccountHostOnlyFlag != cloudProps.DedicatedAccountHostOnlyFlag {
+		err := c.UpgradeInstanceConfig(cid, cloudProps.StartCpus, cloudProps.MaxMemory, 0, cloudProps.DedicatedAccountHostOnlyFlag)
+		if err != nil {
+			return bosherr.WrapError(err, "Upgrading VM")
+		}
+	}
+	return nil
+}
+
+func (c *softLayerVirtualGuestCreator) getUpgradeItemPriceForSecondDisk(id int, diskSize int) (*datatypes.SoftLayer_Product_Item_Price, error) {
+	virtualGuestService, err := c.softLayerClient.GetSoftLayer_Virtual_Guest_Service()
+	if err != nil {
+		return &datatypes.SoftLayer_Product_Item_Price{}, bosherr.WrapError(err, "Creating SoftLayer VirtualGuestService from client")
+	}
+
+	itemPrices, err := virtualGuestService.GetUpgradeItemPrices(id)
+	if err != nil {
+		return &datatypes.SoftLayer_Product_Item_Price{}, err
+	}
+
+	var currentDiskCapacity int
+	var diskType string
+	var currentItemPrice datatypes.SoftLayer_Product_Item_Price
+
+	diskTypeBool, err := virtualGuestService.GetLocalDiskFlag(id)
+	if err != nil {
+		return &datatypes.SoftLayer_Product_Item_Price{}, err
+	}
+
+	if diskTypeBool {
+		diskType = "(LOCAL)"
+	} else {
+		diskType = "(SAN)"
+	}
+
+	for _, itemPrice := range itemPrices {
+		flag := false
+		for _, category := range itemPrice.Categories {
+			if category.CategoryCode == "guest_disk1" {
+				flag = true
+				break
+			}
+		}
+
+		if flag && strings.Contains(itemPrice.Item.Description, diskType) {
+			capacity, err := strconv.ParseInt(itemPrice.Item.Capacity, 10, 0)
+			if err != nil {
+				return &datatypes.SoftLayer_Product_Item_Price{}, err
+			}
+			capToCompare := int(capacity)
+			if capToCompare >= diskSize {
+				if currentItemPrice.Id == -1 || currentDiskCapacity >= capToCompare {
+					currentItemPrice = itemPrice
+					currentDiskCapacity = capToCompare
+				}
+			}
+		}
+	}
+
+	if currentItemPrice.Id == -1 {
+		return &datatypes.SoftLayer_Product_Item_Price{}, bosherr.Errorf("No proper %s disk for size %d", diskType, diskSize)
+	}
+
+	return &currentItemPrice, nil
+}
+
+func getPriceIdForUpgrade(packageItems []datatypes.SoftLayer_Product_Item, option string, value int, public bool) int {
+	for _, item := range packageItems {
+		isPrivate := strings.HasPrefix(item.Description, "Private")
+		for _, price := range item.Prices {
+			if price.LocationGroupId != -1 {
+				continue
+			}
+			if len(price.Categories) == 0 {
+				continue
+			}
+			for _, category := range price.Categories {
+				if item.Capacity != "" {
+					capacity, err := strconv.ParseFloat(item.Capacity, 64)
+					if err != nil {
+						return -1
+					}
+					if !(category.CategoryCode == option && strconv.FormatFloat(capacity, 'f', 0, 64) == strconv.Itoa(value)) {
+						continue
+					}
+					if option == "guest_core" {
+						if public && !isPrivate {
+							return price.Id
+						} else if !public && isPrivate {
+							return price.Id
+						}
+					} else if option == "port_speed" {
+						if strings.Contains(item.Description, "Public") {
+							return price.Id
+						}
+					} else {
+						return price.Id
+					}
+				}
+			}
+		}
+	}
+	return -1
 }
